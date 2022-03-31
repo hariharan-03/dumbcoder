a5d43b93ccb6f281b0cc12e999e3e4cebe528806
hariharan-03 <60616827+hariharan-03@users.noreply.github.com>
2022-03-31 12:43:04 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,9 @@ export default function Home() {
     const handleOpen = () =>{
         if (navigator.share) {
   navigator.share({
-    title: 'web.dev',
-    text: 'Check out web.dev.',
-    url: 'https://web.dev/',
+//     title: 'web.dev',
+    text: 'Refer via',
+    url: 'share this to friend to get free item on https://web.dev/',
   })
     .then(() => console.log('Successful share'))
     .catch((error) => console.log('Error sharing', error));
